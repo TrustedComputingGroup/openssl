17ec50c09daba3b59236cd50f241dfb3734a1844
Jonathan M. Wilbur <jonathan@wilbur.space>
2022-12-13 22:41:04 +0000

    Support attributeDescriptor extension

--- crypto/x509/ext_dat.h
+++ crypto/x509/ext_dat.h
@@ -45,3 +45,4 @@ extern const X509V3_EXT_METHOD ossl_v3_allowed_attribute_assignments;
 extern const X509V3_EXT_METHOD ossl_v3_attribute_mappings;
 extern const X509V3_EXT_METHOD ossl_v3_holder_name_constraints;
 extern const X509V3_EXT_METHOD ossl_v3_indirect_issuer;
+extern const X509V3_EXT_METHOD ossl_v3_attribute_descriptor;
